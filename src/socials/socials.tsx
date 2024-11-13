65976434e609c9e609019ae8f27cfd5466b2661d
Ali <ali.mohseni05@yahoo.se>
2024-11-13 22:44:12 +0100

    added social links and button

--- src/socials/socials.tsx
+++ src/socials/socials.tsx
@@ -1,14 +1,43 @@
-import { FaFileDownload } from "react-icons/fa";
+import {
+  FaEnvelope,
+  FaFileDownload,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
 export function Socials() {
   return (
     <>
-      <a
-        href="#"
-        className="inline-flex items-center hover:bg-white text-white hover:text-[#111111] px-2 py-1 rounded transition duration-200 mr-2 border border-white"
-      >
-        <FaFileDownload size={18} className="mr-2" />
-        Resume
-      </a>
+      <div className="flex space-x-3 items-center">
+        <a
+          href="#"
+          className="inline-flex items-center hover:bg-white text-white hover:text-[#111111] px-2 py-1 rounded transition duration-200 mr-2 border border-white"
+        >
+          <FaFileDownload size={18} className="mr-2" />
+          Resume
+        </a>
+        <a
+          href="https://github.com/alimohseni99"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-400 hover:text-white"
+        >
+          <FaGithub size={24} />
+        </a>
+        <a
+          href="mailto: ali.mohseni05@yahoo.se"
+          className="text-gray-400 hover:text-white"
+        >
+          <FaEnvelope size={24} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ali-mohseni-se/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-400 hover:text-white"
+        >
+          <FaLinkedin size={24} />
+        </a>
+      </div>
     </>
   );
 }
