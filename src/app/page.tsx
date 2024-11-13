4632f62fbadfcc838e3fff899fa3a1c5b34baf68
Ali <ali.mohseni05@yahoo.se>
2024-11-13 23:02:02 +0100

    devided the screen to 40% for socials and profile pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Header } from "@/header/header";
-import { Socials} from "@/socials/socials";
+import { Socials } from "@/socials/socials";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <Socials />
+      <div className="hidden lg:flex flex-col xl:flex-row">
+        <div className="xl:fixed xl:w-2/5 h-screen p-8 xl:p-12 xl:pl-[10%] flex flex-col justify-between min-w-[300px] max-w-[800px]">
+          <div>
+            <Header />
+            <Socials />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
