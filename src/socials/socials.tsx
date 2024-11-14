edc5a8efbfcc7e1b4bdd35dca7b9ada8f46fb42f
Ali Mohseni <ali.mohseni@appliedtechnology.se>
2024-11-14 09:40:11 +0100

    added function to copy email to clipboard

--- src/socials/socials.tsx
+++ src/socials/socials.tsx
@@ -1,42 +1,50 @@
+"use client";
+import Link from "next/link";
 import {
   FaEnvelope,
   FaFileDownload,
   FaGithub,
   FaLinkedin,
 } from "react-icons/fa";
+import copy from "copy-to-clipboard";
+
 export function Socials() {
   return (
     <>
       <div className="flex space-x-3 items-center">
-        <a
+        <Link
           href="#"
           className="inline-flex items-center hover:bg-white text-white hover:text-[#111111] px-2 py-1 rounded transition duration-200 mr-2 border border-white"
         >
           <FaFileDownload size={18} className="mr-2" />
           Resume
-        </a>
-        <a
+        </Link>
+        <Link
           href="https://github.com/alimohseni99"
           target="_blank"
           rel="noopener noreferrer"
           className="text-gray-400 hover:text-white"
         >
           <FaGithub size={24} />
-        </a>
-        <a
-          href="mailto: ali.mohseni05@yahoo.se"
-          className="text-gray-400 hover:text-white"
+        </Link>
+        <span
+          onClick={() => {
+            copy("ali.mohseni05@yahoo.se");
+            alert("Email address copied to clipboard!");
+          }}
+          className="text-gray-400 hover:text-white cursor-pointer"
+          id="email-icon"
         >
           <FaEnvelope size={24} />
-        </a>
-        <a
+        </span>
+        <Link
           href="https://www.linkedin.com/in/ali-mohseni-se/"
           target="_blank"
           rel="noopener noreferrer"
           className="text-gray-400 hover:text-white"
         >
           <FaLinkedin size={24} />
-        </a>
+        </Link>
       </div>
     </>
   );
