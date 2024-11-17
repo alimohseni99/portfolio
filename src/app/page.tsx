b8861d3f600f6902b7b06427ce6f63272620b15f
Ali <ali.mohseni05@yahoo.se>
2024-11-17 21:08:13 +0100

    updated the css where right section disspared for smaller screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,8 +26,9 @@ export default function Home() {
         </div>
 
         {/* Desktop Layout */}
-        <div className="hidden lg:flex flex-col xl:flex-row">
-          <div className="xl:fixed xl:w-2/5 h-screen p-8 xl:p-12 xl:pl-[10%] flex flex-col justify-between min-w-[300px] max-w-[800px]">
+        <div className="flex flex-col lg:flex-row ">
+          {/* Left Section */}
+          <div className="lg:fixed lg:w-2/5 w-full h-auto lg:h-screen p-8 lg:p-12 lg:pl-[10%] flex flex-col justify-between">
             <div>
               <div className="animate-fadeIn delay-200">
                 <Header />
@@ -37,8 +38,17 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="xl:ml-[40%] w-full xl:w-3/5 min-h-screen min-w-[500px] max-w-[1200px] mx-auto">
-            <main className="p-8 xl:p-12 xl:pr-[10%]">
+
+          {/* Right Section */}
+          <div
+            className="w-full lg:w-3/5 min-h-screen p-8 lg:p-12 lg:pr-[10%]"
+            style={{
+              marginLeft: "40%", // make sure that nothing disappears from screen
+              maxWidth: "1200px",
+              minWidth: "300px",
+            }}
+          >
+            <main>
               <div className="animate-slideIn delay-600">
                 <AboutMe />
               </div>
