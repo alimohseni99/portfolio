bed95f812144bd665fc03130fcfe95d44510999b
Ali <ali.mohseni05@yahoo.se>
2024-11-17 21:38:14 +0100

    updated icons for socials

--- src/socials/socials.tsx
+++ src/socials/socials.tsx
@@ -1,51 +1,51 @@
 "use client";
 import copy from "copy-to-clipboard";
 import Link from "next/link";
-import {
-  FaEnvelope,
-  FaFileDownload,
-  FaGithub,
-  FaLinkedin,
-} from "react-icons/fa";
+import { AiOutlineFilePdf } from "react-icons/ai";
+import { FaEnvelope, FaGithub, FaLinkedin } from "react-icons/fa";
 
 export function Socials() {
-  return <>
-    <div className="flex space-x-3 items-center">
-      <Link
-        href="/Cv.pdf"
-        target="_blank"
-        download={"Cv.pdf"}
-        className="inline-flex items-center hover:bg-white text-white hover:text-[#111111] px-2 py-1 rounded transition duration-200 mr-2 border border-white"
-      >
-        <FaFileDownload size={18} className="mr-2" />
-        Resume
-      </Link>
-      <Link
-        href="https://github.com/alimohseni99"
-        target="_blank"
-        rel="noopener noreferrer"
-        className={'text-gray-400 hover:text-white'}
-      >
-        <FaGithub size={24} />
-      </Link>
-      <span
-        onClick={() => {
-          copy("ali.mohseni05@yahoo.se");
-          alert("Email address copied to clipboard!");
-        }}
-        className="text-gray-400 hover:text-white cursor-pointer "
-        id="email-icon"
-      >
-        <FaEnvelope size={24} />
-      </span>
-      <Link
-        href="https://www.linkedin.com/in/ali-mohseni-se/"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-gray-400 hover:text-white"
-      >
-        <FaLinkedin size={24} />
-      </Link>
-    </div>
-  </>;
+  return (
+    <>
+      <div className="flex space-x-3 items-center">
+        <Link
+          href="/Cv.pdf"
+          target="_blank"
+          download={"Cv.pdf"}
+          className="text-gray-400 cursor-pointer y-white hover:text-[#ffffff] px-2 py-1 rounded transition duration-200"
+          title="My Resume"
+        >
+          <AiOutlineFilePdf size={24} />
+        </Link>
+        <Link
+          href="https://github.com/alimohseni99"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-400 cursor-pointer y-white hover:text-[#ffffff] px-2 py-1 rounded transition duration-200 "
+          title="GitHub Profile"
+        >
+          <FaGithub size={24} />
+        </Link>
+        <span
+          onClick={() => {
+            copy("ali.mohseni05@yahoo.se");
+            alert("Email address copied to clipboard!");
+          }}
+          className="text-gray-400 y-white hover:text-[#ffffff] px-2 py-1 rounded transition duration-200 cursor-pointer"
+          title="My email"
+        >
+          <FaEnvelope size={24} />
+        </span>
+        <Link
+          href="https://www.linkedin.com/in/ali-mohseni-se/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-400 cursor-pointer y-white hover:text-[#ffffff] px-2 py-1 rounded transition duration-200"
+          title="Linkedin Profile"
+        >
+          <FaLinkedin size={24} />
+        </Link>
+      </div>
+    </>
+  );
 }
